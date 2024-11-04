dd422c57de962e6545006647e204ba09b389cf6b
unknown <mujahidmalikarain@gmail.com>
2024-11-04 09:10:50 -0800

    update data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { updateInvoice } from '@/app/lib/actions';
+import { notFound } from 'next/navigation';
+ 
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+ 
+  if (!invoice) {
+    notFound();
+  }
+ 
+  // ...
+}
