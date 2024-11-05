7faa4079a27b1a4d107079410af4a093abf391bb
unknown <mujahidmalikarain@gmail.com>
2024-11-05 05:42:00 -0800

    impment sign in and log out

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/app/ui/global.css';
 
 import {inter} from '@/app/ui/font';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'fintech Dashboard',
+  description: 'code and imagination fintech app.',
+ 
+};
 export default function RootLayout({
   children,
 }: {
